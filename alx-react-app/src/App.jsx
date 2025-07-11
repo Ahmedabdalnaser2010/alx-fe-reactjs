c785765ca870d8801d19675e08b9dc3da36bd244
ahmed <ahmedabdelnaser3010@gmail.com>
2025-07-11 15:16:35 +0300

    task 4

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,15 +2,14 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 
 function App() {
 
 
   return (
     <>
-      <Header />
-      <MainContent />
-      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 
     </>
   )
