74c8a5c8bb41bf22fef47ed52c70a945ab8ce96d
ahmed <ahmedabdelnaser3010@gmail.com>
2025-07-19 23:41:34 +0300

    company task

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <footer style={{
+            backgroundColor: '#333',
+            color: 'white',
+            textAlign: 'center',
+            padding: '1rem',
+            marginTop: '2rem'
+        }}>
+            <p>© 2023 My Company. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
